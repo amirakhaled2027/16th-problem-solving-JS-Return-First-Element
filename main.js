25c26f9f75f4a7d698a549f76102869a672f7333
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-06-07 01:54:16 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//Create a function that takes an array containing only numbers and return the first element
+var arr = ['amira', 'khaled', 'ahmed', 'saad'];
+function getFirstElement(arr){
+  return arr[0];
+}
+console.log(getFirstElement(arr));
